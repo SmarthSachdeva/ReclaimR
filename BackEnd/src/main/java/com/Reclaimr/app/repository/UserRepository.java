bfd3b8308f78090a17eca968f549bccd80169e0a
Smarth Sachdeva <smarthsachdeva1632@gmail.com>
2024-08-24 00:35:13 +0530

    Mongo Db atlas connected

--- BackEnd/src/main/java/com/Reclaimr/app/repository/UserRepository.java
+++ BackEnd/src/main/java/com/Reclaimr/app/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.Reclaimr.app.repository;
 
 import com.Reclaimr.app.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserRepository extends MongoRepository<User, String> {
 }
