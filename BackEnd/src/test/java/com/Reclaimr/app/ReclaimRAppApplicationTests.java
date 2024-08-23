27289cc484ea15725ccb298cc264178375471eb3
Smarth Sachdeva <smarthsachdeva1632@gmail.com>
2024-08-23 12:22:32 +0530

    Backend setup done

---
+++ BackEnd/src/test/java/com/Reclaimr/app/ReclaimRAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Reclaimr.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclaimRAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
